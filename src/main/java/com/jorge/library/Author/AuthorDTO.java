fd38e6adbf417b91e2bcfc945fccf9e0d38a268a
Ctrlahr <ctrlahrium@gmail.com>
2026-01-12 23:33:24 -0300

    Finish the Author DTO and Mapper

--- src/main/java/com/jorge/library/Author/AuthorDTO.java
+++ src/main/java/com/jorge/library/Author/AuthorDTO.java
@@ -1,5 +1,19 @@
 package com.jorge.library.Author;
 
+import com.jorge.library.Book.BookModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorDTO {
+
+    private Long id;
+    private String name;
+    private int age;
+    private List<BookModel> books;
 
 }
--- src/main/java/com/jorge/library/Author/AuthorMapper.java
+++ src/main/java/com/jorge/library/Author/AuthorMapper.java
@@ -1,4 +1,50 @@
 package com.jorge.library.Author;
 
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class AuthorMapper {
+
+    public AuthorDTO toDTO(AuthorModel author) {
+        if (author == null) {
+            return null;
+        }
+
+        AuthorDTO dto = new AuthorDTO();
+
+        dto.setId(author.getId());
+        dto.setAge(author.getAge());
+        dto.setName(author.getName());
+        dto.setBooks(author.getBooks());
+
+        return dto;
+
+    }
+
+    public AuthorModel toEntity(AuthorDTO dto) {
+
+        if (dto == null) {
+            return null;
+        }
+
+        AuthorModel author = new AuthorModel();
+
+        author.setId(dto.getId());
+        author.setAge(dto.getAge());
+        author.setName(dto.getName());
+        author.setBooks(dto.getBooks());
+
+        return author;
+    }
+
+    public List<AuthorDTO> toDTOList(List<AuthorModel> author) {
+
+        return author.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
+    }
+
 }
