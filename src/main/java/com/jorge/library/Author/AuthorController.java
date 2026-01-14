4edf467887cbd1faaa6eae3c2f26abc2c52b46b8
Ctrlahr <ctrlahrium@gmail.com>
2026-01-13 22:40:07 -0300

    Add an endpoint to list all authors

--- src/main/java/com/jorge/library/Author/AuthorController.java
+++ src/main/java/com/jorge/library/Author/AuthorController.java
@@ -4,17 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PrivateKey;
+import java.util.List;
+
 @RestController
 @RequestMapping("author")
 public class AuthorController {
 
-//    Endpoints
+    private AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    //    Endpoints
 
 //    Test Endpoint
     @GetMapping("test")
     public String authorTest() {return "Working!!";}
 
 //    Get all authors
+    @GetMapping("listAuthors")
+    public List<AuthorDTO> listAuthors() {
+        return authorService.listAuthors();
+    }
 
 
 }
--- src/main/java/com/jorge/library/Author/AuthorService.java
+++ src/main/java/com/jorge/library/Author/AuthorService.java
@@ -1,8 +1,30 @@
 package com.jorge.library.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class AuthorService {
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private AuthorMapper authorMapper;
+
+    public AuthorService(AuthorRepository authorRepository, AuthorMapper authorMapper) {
+        this.authorRepository = authorRepository;
+        this.authorMapper = authorMapper;
+    }
+
+//    Métodos
+    public List<AuthorDTO> listAuthors(){
+        List<AuthorModel> author = authorRepository.findAll();
+        return author.stream()
+                .map(authorMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
