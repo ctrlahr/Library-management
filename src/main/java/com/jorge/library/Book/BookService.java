1239752589891f3c0597263a2b8f3b6bb685cbda
Ctrlahr <ctrlahrium@gmail.com>
2026-01-26 22:56:32 -0300

    Add the book service and create a list all books method

---
+++ src/main/java/com/jorge/library/Book/BookService.java
@@ -0,0 +1,37 @@
+package com.jorge.library.Book;
+
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.sql.model.ast.builder.CollectionRowDeleteByUpdateSetNullBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    public BookService(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
+    }
+
+
+//    List all books
+    public List<BookDTO> listBooks() {
+        List<BookModel> books = bookRepository.findAll();
+        return books.stream()
+                .map(bookMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+
+
+}
