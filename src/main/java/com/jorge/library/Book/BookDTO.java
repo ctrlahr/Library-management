f229f26034ec9e6aa3881e725e207e101f658105
Ctrlahr <ctrlahrium@gmail.com>
2026-01-26 22:29:30 -0300

    Add a toDTO and toEntity map to BookMapper

--- src/main/java/com/jorge/library/Book/BookDTO.java
+++ src/main/java/com/jorge/library/Book/BookDTO.java
@@ -1,4 +1,18 @@
 package com.jorge.library.Book;
 
+import com.jorge.library.Author.AuthorDTO;
+import com.jorge.library.Author.AuthorModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDTO {
+
+    private Long id;
+    private String name;
+    private AuthorModel author;
+
 }
--- src/main/java/com/jorge/library/Book/BookMapper.java
+++ src/main/java/com/jorge/library/Book/BookMapper.java
@@ -1,4 +1,37 @@
 package com.jorge.library.Book;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookMapper {
+
+    public BookDTO toDTO(BookModel book) {
+        if (book == null) {
+            return null;
+        }
+
+        BookDTO dto = new BookDTO();
+
+        dto.setName(book.getName());
+        dto.setId(book.getId());
+        dto.setAuthor(book.getAuthor());
+
+        return dto;
+
+    }
+
+    public BookModel toEntity(BookDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        BookModel book = new BookModel();
+
+        book.setName(dto.getName());
+        book.setId(dto.getId());
+        book.setAuthor(dto.getAuthor());
+
+        return book;
+    }
+    
 }
