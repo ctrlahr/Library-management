b85a4aa5a139188431508eca78d7b85ccfcd6677
Ctrlahr <ctrlahrium@gmail.com>
2026-02-12 21:45:44 -0300

    Remove the old fetch code from the js

--- src/main/resources/static/js/scriptTeste.js
+++ src/main/resources/static/js/scriptTeste.js
@@ -1,32 +1,3 @@
-// async function chamarAPI() {
-//     const username = "admin"
-//     const password = "admin"
-//     const credentials = btoa(`${username}:${password}`);
-    
-// // const response = await fetch('http://localhost:8080/author/listAll', {
-// //     method: 'GET',
-// //     headers: {
-// //         'Content-Type': 'application/json',
-// //         'Authorization': `Basic ${credencials}`
-// //     }
-// // });
-// // const autores = await response.json();
-// // console.log(autores);
-// // }
-
-// // fetch('http://localhost:8080/author/listAll', {
-// //     method: 'GET',
-// //     headers: {
-// //         'Authorization': `Basic ${credentials}`,
-// //     }
-// // })
-
-
-// }
-
-// chamarAPI();
-
-
 async function callAPI() {
     const username = "admin";
     const password = "admin";
