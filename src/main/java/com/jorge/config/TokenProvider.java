4bc17e0ea79b3c0a80ba0333aa036bde9a6bb4ac
Jorge <ctrlahrium@gmail.com>
2026-07-13 06:33:51 -0300

    Feat: create the initial function to build the token

--- src/main/java/com/jorge/config/TokenProvider.java
+++ src/main/java/com/jorge/config/TokenProvider.java
@@ -1,4 +1,34 @@
 package com.jorge.config;
 
+import io.jsonwebtoken.Jwt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenProvider {
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    @Value("${jwt.key}")
+    private String key;
+
+
+//    Generate Token
+    public String generateToken(Authentication authentication) {
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        return buildToken(user.getUsername());
+    }
+
+    private String buildToken(UserDetails username) {
+
+    }
+
+//    Validate token
+
+
+//    Extract information about the token
+
 }
