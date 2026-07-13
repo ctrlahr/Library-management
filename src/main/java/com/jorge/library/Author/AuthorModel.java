09d58cacb9a9e4419dfa3cb8a8d35bba41043fa2
Jorge <ctrlahrium@gmail.com>
2026-07-13 05:43:54 -0300

    Feature: Add to AuthorModel the UserDetails getters

--- src/main/java/com/jorge/library/Author/AuthorModel.java
+++ src/main/java/com/jorge/library/Author/AuthorModel.java
@@ -2,12 +2,16 @@ package com.jorge.library.Author;
 
 import com.jorge.library.Book.BookModel;
 import com.jorge.library.roles.RolesModel;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Fetch;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,7 +21,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthorModel {
+public class AuthorModel implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
@@ -25,6 +29,9 @@ public class AuthorModel {
 
     @Column(name = "Name")
     private String name;
+
+    @Column(name = "email")
+    private String email;
 
     @Column(name = "Age")
     private int age;
@@ -43,6 +50,38 @@ public class AuthorModel {
             inverseJoinColumns = @JoinColumn(name = "roleId"))
     private Set<RolesModel> roles = new HashSet<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
 
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
