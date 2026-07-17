79243541e083d621a12a592db52aa553bcff1403
Jorge <ctrlahrium@gmail.com>
2026-07-17 05:53:44 -0300

    feat: add to token provider class functions to build the jwt token, get the sign key, verify if the token is valid, get claims and get the username from the claims

--- src/main/java/com/jorge/config/TokenProvider.java
+++ src/main/java/com/jorge/config/TokenProvider.java
@@ -1,10 +1,16 @@
 package com.jorge.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class TokenProvider {
@@ -22,13 +28,44 @@ public class TokenProvider {
         return buildToken(user.getUsername());
     }
 
-    private String buildToken(UserDetails username) {
+    private String buildToken(String username) {
+        Date now  = new Date();
+        Date expiration = new Date(now.getTime() + expirationTime);
 
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(key.getBytes());
     }
 
 //    Validate token
+    public boolean isTokenValid(String token) {
+        try {
+            getClaims(token); /*Try to get the JWT payload*/
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
 
+    private Claims getClaims(String token) {
+//        Validate signature
+//        Validate expiration
+        return Jwts.parser()
+                .verifyWith(getSigningKey()) /*Verify the signature key*/
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
 //    Extract information about the token
-
+    private String getUsername(String token) {
+        return getClaims(token).getSubject();
+    }
 }
